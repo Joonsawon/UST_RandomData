f7b7be739a0bdfc1a662ca603680b2bb62b71eeb
joonsawon <joonsawon@gmail.com>
2023-12-01 15:36:37 +0900

    과제 진행함2

--- temp.py
+++ temp.py
@@ -13,7 +13,7 @@ This is a temporary script file."""
 
 !git push -u origin main
 !git add -A
-!git commit -m "주피터노트북 파일 추가"
+!git commit -m "과제 진행함"
 !git push origin main
 !git remote -v  # 원격저장소 확인
 !git pull #원격저장소 가져오기
