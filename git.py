6488caffb9cac9c45035bf5ae09cbc548df2e229
joonsawon <joonsawon@gmail.com>
2023-12-11 16:59:40 +0900

    한달 데이터에 STFT 적용 분석 중

--- git.py
+++ git.py
@@ -10,7 +10,7 @@ This is a temporary script file."""
 
 #!git push -u origin main
 !git add -A
-!git commit -m "주가 데이터 앙상블 평균 계산. Nonstationary process 판정"
+!git commit -m "한달 데이터에 STFT 적용 분석 중"
 !git push origin main
 # 원격저장소 확인
 
